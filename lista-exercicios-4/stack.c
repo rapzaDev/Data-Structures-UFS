c0dbe8768a3f19601f17cee1d5cd8c82d0fc02db
Rafael Pérez <rapzadev@gmail.com>
2020-11-12 18:02:45 -0300

    Update on lista-exericios-4 code

---
+++ lista-exercicios-4/stack.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include"stack.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ lista-exercicios-4/stack.h
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+/**
+ * Creates an instancy of stack cell.
+**/
+typedef struct Pilha {
+    int32_t value;
+    struct Pilha *prox;
+}Stack;
+
+/**
+ * Creates the stack Head cell and returns it.
+**/
+Stack * createStack(Stack *pHead) {
+    pHead =(Stack*) malloc(sizeof(Stack));
+        pHead->value=0;
+        pHead->prox=NULL;
+    return pHead;
+}
+
+/**
+ * Stack another cell whit the value parameter in the Stack ED. 
+   The second parameter always be the stack head's pointer.
+**/
+void stackPush(int32_t value, Stack *pHead) { 
+    Stack *newStackCell;
+    newStackCell = (Stack*)malloc(sizeof(Stack));
+        newStackCell->value=value;
+        newStackCell->prox=pHead->prox;
+    pHead->prox=newStackCell;
+}
+
+/**
+ * Pulls the first cell in the head of the Stack an return his value.
+**/
+int32_t stackPull(Stack *pHead) {
+    int32_t value=0;
+    Stack *markedStackCell;
+    markedStackCell = (Stack*)malloc(sizeof(Stack));
+        markedStackCell = pHead->prox;
+        value = markedStackCell->value;
+    pHead->prox=markedStackCell->prox;
+    
+    free(markedStackCell);
+    return value;
+}
+
+/**
+ * Prints the Stack ED starting by the top of the Stack
+**/
+void stackPrint(Stack *pHeadProx) {
+    if(!pHeadProx) return;
+    if(pHeadProx->prox == NULL){
+        printf("%d\n", pHeadProx->value);    
+    }
+    printf("%d ", pHeadProx->value);
+    stackPrint(pHeadProx->prox);
+}
